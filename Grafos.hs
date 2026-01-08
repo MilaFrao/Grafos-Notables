f3e63230efed6ff5bc7543e865e5f065f808596b
Samuel <miladelarocaguerra@gmail.com>
2026-01-08 12:12:17 -0400

    Proyecto terminado Falta documentar y revisar (Este proyecto se hizo con un poco de desgana)

--- Grafos.hs
+++ Grafos.hs
@@ -9,6 +9,9 @@
 
 module Main where
 
+import Data.Maybe (mapMaybe)
+import Text.Read (readMaybe)
+
 type Vertice = Int
 type Grafo = [[Vertice]]
 
@@ -27,6 +30,12 @@ agregarArista g a b =
             | otherwise = v
     in zipWith agregar [0..] g  
 
+-- Acceso seguro a vecinos (evita `!!` fuera de rango)
+vecinos :: Grafo -> Vertice -> [Vertice]
+vecinos g v
+    | v >= 0 && v < length g = g !! v
+    | otherwise              = []
+
 -- =========================
 -- DFS con detección de ciclos
 -- =========================
@@ -34,10 +43,10 @@ agregarArista g a b =
 dfsArbol :: Grafo -> Vertice -> Vertice -> [Vertice] -> Bool
 dfsArbol g v padre visitados =
     let visitados' = v : visitados
-    in any (tieneCiclo visitados') (g !! v)
+    in any (tieneCiclo visitados') (vecinos g v)
     where
     tieneCiclo vis u
-        | u == padre   = False        -- CORREGIDO
+        | u == padre   = False
         | u `elem` vis = True
         | otherwise    = dfsArbol g u v vis
 
@@ -47,9 +56,9 @@ dfsArbol g v padre visitados =
 
 dfs :: Grafo -> Vertice -> [Vertice] -> [Vertice]
 dfs g v visitados
-    | v `elem` visitados = visitados      -- CORREGIDO
+    | v `elem` visitados = visitados
     | otherwise =
-        foldl (\vis u -> dfs g u vis) (v : visitados) (g !! v)
+        foldl (\vis u -> dfs g u vis) (v : visitados) (vecinos g v)
 
 -- =========================
 -- Propiedades del grafo
@@ -79,15 +88,41 @@ esBipartito :: Grafo -> Bool
 esBipartito g = all (bfsBipartito g) [0 .. length g - 1]
 
 bfsBipartito :: Grafo -> Vertice -> Bool
-bfsBipartito g inicio = bfs [(inicio, 0)] []     -- CORREGIDO
+bfsBipartito g inicio = bfs [(inicio, 0)] []
     where 
     bfs [] _ = True 
     bfs ((v, c):cola) colores = 
         case lookup v colores of
             Just c' -> c == c' && bfs cola colores
             Nothing ->
-                let nuevos = [(u, 1 - c) | u <- g !! v]
-                in bfs (nuevos ++ cola) ((v, c) : colores)  -- CORREGIDO
+                let nuevos = [(u, 1 - c) | u <- vecinos g v]
+                in bfs (nuevos ++ cola) ((v, c) : colores)
+
+-- =========================
+-- Lectura del archivo data.io
+-- =========================
+
+leerGrafoDesdeArchivo :: FilePath -> IO Grafo
+leerGrafoDesdeArchivo ruta = do
+    contenido <- readFile ruta
+    let ls = lines contenido
+
+    case ls of
+        [] -> return []
+        (nLine:rest) ->
+            case readMaybe nLine :: Maybe Int of
+                Nothing -> error "Formato inválido: número de vértices"
+                Just n ->
+                    let aristas = mapMaybe parseArista rest
+                        grafoInicial = crearGrafo n
+                        grafoFinal = foldl (\g (u,v) -> agregarArista g u v) grafoInicial aristas
+                    in return grafoFinal
+
+parseArista :: String -> Maybe (Vertice, Vertice)
+parseArista linea =
+    case mapM readMaybe (words linea) of
+        Just [u,v] -> Just (u,v)
+        _ -> Nothing
 
 -- =========================
 -- MAIN
@@ -95,15 +130,11 @@ bfsBipartito g inicio = bfs [(inicio, 0)] []     -- CORREGIDO
 
 main :: IO ()
 main = do
-    let g0 = crearGrafo 4
-        g1 = agregarArista g0 0 1
-        g2 = agregarArista g1 1 2
-        g3 = agregarArista g2 2 3
-        g4 = agregarArista g3 3 0
-        
-    putStrLn ("Grafo g4 es conexo: " ++ show (esConexo g4))
-    putStrLn ("Grafo g4 es arbol: " ++ show (esArbol g4))
-    putStrLn ("Grafo g4 es euleriano: " ++ show (esEuleriano g4))
-    putStrLn ("Grafo g4 es bipartito: " ++ show (esBipartito g4))
+    grafo <- leerGrafoDesdeArchivo "data.io"
+
+    putStrLn ("Grafo es conexo: " ++ show (esConexo grafo))
+    putStrLn ("Grafo es arbol: " ++ show (esArbol grafo))
+    putStrLn ("Grafo es euleriano: " ++ show (esEuleriano grafo))
+    putStrLn ("Grafo es bipartito: " ++ show (esBipartito grafo))
 
 ---------------------------------- Fin de Grafos completos A ver que tal -----------------------------------------
